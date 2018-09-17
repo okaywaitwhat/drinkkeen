f8148ebf16f072a6a0ba9302fb9a6c2a870a1127
GMaiolo <guille.maiolo@gmail.com>
2018-09-17 19:32:49 -0300

    fixes para el form

--- web/src/components/RegistrationForm.vue
+++ web/src/components/RegistrationForm.vue
@@ -1,25 +1,26 @@
 <template>
-    <form class="p-4 bg-light rounded">
+  <div class="p-4 bg-light rounded">
     <div class="form-group">
-      <label for="exampleInputEmail1">Nombre de usuario</label>
-      <input type="email" class="form-control font-weight-light" id="exampleInputEmail1" aria-describedby="userNamelHelp" placeholder="Ej: beerkeen">
+      <label for="username">Nombre de usuario</label>
+      <input type="text" class="form-control font-weight-light" id="username" placeholder="Ej: beerkeen">
     </div>
     <div class="form-group">
-      <label for="exampleInputEmail1">Email</label>
-      <input type="email" class="form-control font-weight-light" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Ej: alguien@ejemplo.com">
+      <label for="email">Email</label>
+      <input type="email" class="form-control font-weight-light" id="email" placeholder="Ej: alguien@ejemplo.com">
     </div>
     <div class="form-group">
-      <label for="exampleInputPassword1">Contraseña</label>
-      <input type="password" class="form-control font-weight-light" id="exampleInputPassword1" placeholder="Contraseña">
-      <small id="emailHelp" class="form-text text-muted font-weight-light">Y la de da de da de da de day oh!</small>
+      <label for="password">Contraseña</label>
+      <input type="password" class="form-control font-weight-light" id="password" placeholder="Contraseña">
+      <small class="form-text text-muted font-weight-light">Y la de da de da de da de day oh!</small>
     </div>
-    <button type="submit" class="btn btn-dark">Registrarme</button><small id="emailHelp" class="form-text text-muted font-weight-light">¿Ya tenés cuenta? <u>Ingresa acá</u>.</small>
-  </form>
+    <button class="btn btn-dark">Registrarme</button>
+    <small class="form-text text-muted font-weight-light">¿Ya tenés cuenta? <u>Ingresa acá</u>.</small>
+  </div>
 </template>
 
 <script>
   export default {
-  name: 'registrationform',
+  name: 'registrationform'
 }
 </script>
 
